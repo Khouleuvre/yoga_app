4461e16da7fd66d79289dba8680a7cd867bc334f
Manu Labricole <manu_barriol@hotmail.fr>
2023-11-15 11:29:35 +0100

    finish embedder

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import os
 from streamClassifier.embeddings import StreamEmbedder
 
 
-
 # Variables
 cwd = os.getcwd()
 image_in_dir = os.path.join(cwd, "assets", "images", "stream_in")
 image_out_dir = os.path.join(cwd, "assets", "images", "stream_out")
-csv_out_dir = os.path.join(cwd, "assets", "stream", "csv")
+csv_out_dir = os.path.join(cwd, "assets", "csv", "stream")
 
 stream_embedder = StreamEmbedder(
     stream_image_in_dir=image_in_dir,
     stream_image_out_dir=image_out_dir,
     stream_csv_out_dir=csv_out_dir,
 )
+
+stream_embedder.generate_embbedings()
--- streamClassifier/embeddings.py
+++ streamClassifier/embeddings.py
@@ -17,11 +17,10 @@ class StreamEmbedder:
             images_out_folder=stream_image_out_dir,
             csvs_out_folder=stream_csv_out_dir,
         )
-        self._is_image_in_dir()
 
     def _is_unique_image_in_dir(self):
         """
-        Returns true if the image is in the directory
+        Returns true if the image is in the directory and there is only one (the last one to treat)
         """
 
         _unique_folder = os.listdir(self.stream_image_in_dir)[0]
@@ -29,27 +28,43 @@ class StreamEmbedder:
         files = os.listdir(_unique_folder_path)
 
         if len(files) == 1:
-            print("There are images in the directory")
+            # print("There are images in the directory")
             return True
         else:
-            print("There are no images in the directory")
+            # print("There are no images in the directory")
             return False
 
-    def _remove_image_from_in_dir(self, image_name):
-        pass
+    def _clean_directory(self, directory: str) -> None:
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
 
-    def get_embeddings_from_stream(self, stream):
+    def _remove_image_from_in_out_dir(self) -> None:
+        target_dir_to_clean_in = os.path.join(self.stream_image_in_dir, "stream")
+        target_dir_to_clean_out = os.path.join(self.stream_image_out_dir, "stream")
+        self._clean_directory(directory=target_dir_to_clean_in)
+        self._clean_directory(directory=target_dir_to_clean_out)
+
+    def _get_embeddings_from_stream(self):
         """
         Returns the embeddings from the stream
         """
 
-        is_unique_image_in_dir = self._is_image_in_dir()
+        if self._is_unique_image_in_dir():
+            print("There is ONE image only in the directory")
+        else:
+            return false
 
-        pass
+    def generate_embbedings(self) -> None:
+        """
+        Returns the embeddings from the stream
+        """
 
-
-def get_embeddings_from_image():
-    """
-    Returns the embeddings from the image
-    """
-    pass
+        self.bootstrap_helper.bootstrap()
+        self._remove_image_from_in_out_dir()
